873cb6886444bc72d36fa0e2e2359460552f1578
Sandra <sandra.alpha2010@gmail.com>
2021-04-19 16:30:41 -0700

    change serverjs to add code for Apollo Server and added a schemas folder

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require("./config/connection");
 
 //const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
@@ -21,6 +34,9 @@ app.use(express.json());
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
 
